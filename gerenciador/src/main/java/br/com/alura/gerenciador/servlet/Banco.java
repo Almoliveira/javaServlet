28e60841b9a9ba4cc8c1b8f9cd2a214085829d98
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-09-27 05:35:12 -0300

    Aula 7: Adicionado remover empresa!

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -1,17 +1,21 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
-	
+	private static Integer chaveSequencial = 1;
 	
 	static {
 		Empresa empresa = new Empresa();
+		empresa.setId(chaveSequencial++);
 		empresa.setNome("Alura");
+		empresa.setId(chaveSequencial++);
 		Empresa empresa2 = new Empresa();
+		empresa2.setId(chaveSequencial++);
 		empresa2.setNome("Caelum");
 		lista.add(empresa);
 		lista.add(empresa2);
@@ -20,6 +24,7 @@ public class Banco {
 
 	public void adiciona(Empresa empresa) {
 		// TODO Auto-generated method stub
+		empresa.setId(Banco.chaveSequencial++);
 		lista.add(empresa);
 		
 		
@@ -28,4 +33,17 @@ public class Banco {
 	public List<Empresa> getEmpresas(){
 		return Banco.lista;
 	}
+
+	public void removeEmpresa(Integer id) { 
+		
+		Iterator<Empresa> it = lista.iterator();
+		
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			if(emp.getId() == id ) {
+		           it.remove();
+		        }
+		}
+		
+	} 
 }
