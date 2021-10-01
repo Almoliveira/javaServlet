1c6dbc0c31631619294f0283d118508fd82efe20
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-10-01 04:36:19 -0300

    Aula 3: Finalizada

--- gerenciador/src/main/java/br/com/alura/gerenciador/acao/Login.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/acao/Login.java
@@ -6,6 +6,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Usuario;
+
 public class Login implements Acao {
 
 	@Override
@@ -17,8 +20,18 @@ public class Login implements Acao {
 		
 		System.out.println("Logando: " + login);
 		
+		Banco banco = new Banco();
+		Usuario usuario = banco.existeUsuario(login, senha);
 		
-		return "redirect:entrada?acao=ListaEmpresas";
+		
+		if(usuario != null) {
+			System.out.println("Usuario existe!");
+			return "redirect:entrada?acao=ListaEmpresas";
+		}else {
+			return "redirect:entrada?acao=LoginForm";
+		}
+		
+		
 	}
 
 }
